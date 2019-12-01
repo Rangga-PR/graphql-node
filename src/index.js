c1e05c38ebdb7f7e4aca4d9a0f275b689b2d1f31
ranggaraharja <rangga.raharja@ralali.com>
2019-12-01 17:57:03 +0700

    setup express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello world haha")
+import express from "express";
+
+const server = async () => {
+    const app = express();
+    app.get('/', (req, res) => res.send("hello from express") );
+
+    app.listen(3001);
+}
+
+server();
