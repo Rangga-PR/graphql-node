70cc3d1f97cdf08aae10f8277e70895fa12f1819
ranggaraharja <rangga.raharja@ralali.com>
2019-12-01 19:04:22 +0700

    setup graphql server using apollo-server-express

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { resolvers } from "./graphql/resolvers";
+import { typeDefs } from "./graphql/typeDefs";
 
 const server = async () => {
     const app = express();
-    app.get('/', (req, res) => res.send("hello from express") );
+    const GraphqlServer = new ApolloServer({
+        typeDefs,
+        resolvers
+    });
 
+    GraphqlServer.applyMiddleware({ app });
     app.listen(3001);
 }
 
